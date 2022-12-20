336e0b60bda0576c457314718e78a2558eb6a020
soulthanti04 <fadhlillahbagussoulthan@gmail.com>
2022-12-20 13:12:58 +0700

    praktikum-11

---
+++ praktikum-11/assets/js/script.js
@@ -0,0 +1,60 @@
+// isi logic gamemu disini
+// menangkap elemernt a dengan menggunakan looping
+document.querySelectorAll('#option a').forEach((anchor)=> {
+    anchor.addEventListener('click', (element) => {
+        computerChoice(element);
+    })
+});
+
+function computerChoice(element){
+    // menangkap pilihan user
+    let pilihanUser = element.target.innerText;
+
+    //menangkap pilihan komouter pada result
+    let pilihanKomputer = document.querySelector('#result');
+
+    //menangkap hasil pertandingan
+    let hasil = document.querySelector('#result2');
+
+    // membuat pilihan untuk komputer
+    let choices = ['Rock', 'Paper', 'Scissors'];
+
+    // membuat pilihan random untuk komputer
+    pilihanKomputer.innerHTML = choices[Math.floor(Math.random() * choices.length)];
+    pilihanKomputer = pilihanKomputer.innerHTML;
+
+
+    // jika user yang menang
+    if (pilihanUser == 'Rock' && pilihanKomputer == 'Scissors'){
+        hasil.innerHTML = ('You Win');
+    }
+
+    if (pilihanUser == 'Paper' && pilihanKomputer == 'Rock'){
+        hasil.innerHTML = ('You Win');
+    }
+
+    if (pilihanUser == 'Scissors' && pilihanKomputer == 'Paper'){
+        hasil.innerHTML = ('You Win');
+    }
+
+    // jika komputer yang menang
+    if (pilihanUser == 'scissors' && pilihanKomputer == 'Rock'){
+        hasil.innerHTML = ('You Lose');
+    }
+
+    if (pilihanUser == 'Rock' && pilihanKomputer == 'Paper'){
+        hasil.innerHTML = ('You Lose');
+    }
+
+    if (pilihanUser == 'Paper' && pilihanKomputer == 'Scissors'){
+        hasil.innerHTML = ('You Lose');
+    }
+
+    // jika komputer dan user draw
+    if(pilihanUser == pilihanKomputer){
+        hasil.innerHTML = ('You Draw');
+    }
+
+
+
+}
